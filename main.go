b3b7aed06c2d33d0d2046f8e7d86623498aa5676
Jiff <102629485+jifpbj@users.noreply.github.com>
2025-09-25 10:31:59 -0700

    add commands to stdin

--- main.go
+++ main.go
@@ -1,28 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-	"bufio"
-	"os"
-)
-
 func main() {
-
-	
-	for {
-		fmt.Print("Pokedex > ")
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		text := scanner.Text()
-		words := cleanInput(text)
-		fmt.Println("Your command was:", words[0])
-	}
-}
-
-func cleanInput(text string) []string {
-	output := strings.ToLower(text)
-	words := strings.Fields(output)
-	return words
+	startRepl()
 }
 
